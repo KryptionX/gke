fd659c2ce57a4c30827d30bcfff0f311a25f5d33
Johnny Gossette <johnny@hashicorp.com>
2019-11-12 10:44:50 -0700

    added content

--- gke-cluster/gke.tf
+++ gke-cluster/gke.tf
@@ -1,3 +1,25 @@
 module "m" {
   source = "./gke-cluster"
 }
+
+terraform {
+  backend "remote" {
+    hostname     = "legible-bee.hashicorp-success.com"
+    organization = "HershiKerp"
+
+    workspaces {
+      name = "gke"
+    }
+  }
+}
+
+resource "null_resource" "example" {
+  triggers = {
+    stuff = "${timestamp()}"
+  }
+}
+resource "null_resource" "GENIUS" {
+  triggers = {
+    stuff = "${timestamp()}"
+  }
+}
